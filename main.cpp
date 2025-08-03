730f87c6e0404b4427d6586c53ef0eeab940bf0b
Dominika Mazur <92177055+DominikaMa3ur@users.noreply.github.com>
2025-08-03 20:41:03 +0200

    Cells with missing neighbours (edge)
    + cells displayed as rectangles instead of text

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include "raylib.h"
+#include <climits>
+
+const int EDGE_NEIGHBOUR = INT_MAX;
+
+const int CELLS_X = 20;
+const int CELLS_Y = 20;
+
+Color COLORS[2] = {Color{0,0,0,255},Color{255,255,255,255}};
 
 struct NEIGHBOURS4 {
     int TOP;
@@ -16,30 +24,31 @@ int countNeighbours(NEIGHBOURS4 neighbours, int value) {
     return count;
 }
 
-NEIGHBOURS4 returnNeighbours(int (&cell)[10][10], int x, int y) {
+NEIGHBOURS4 returnNeighbours(int (&cell)[CELLS_X][CELLS_Y], int x, int y) {
     NEIGHBOURS4 returned;
-    returned.TOP = cell[x][y-1];
-    returned.BOTTOM = cell[x][y+1];
-    returned.LEFT = cell[x-1][y];
-    returned.RIGHT = cell[x+1][y];
+    if (y == 0) returned.TOP = EDGE_NEIGHBOUR;
+    else returned.TOP = cell[x][y-1];
+    if (y == CELLS_Y-1) returned.BOTTOM = EDGE_NEIGHBOUR;
+    else returned.BOTTOM = cell[x][y+1];
+    if (x == 0) returned.LEFT = EDGE_NEIGHBOUR;
+    else returned.LEFT = cell[x-1][y];
+    if (x == CELLS_X-1) returned.RIGHT = EDGE_NEIGHBOUR;
+    else returned.RIGHT = cell[x+1][y];
     return returned;
 }
 
-const int CELLS_X = 10;
-const int CELLS_Y = 10;
-
-void fillWithZeros (int (&cell)[10][10])
+void fillWithZeros (int (&cell)[CELLS_X][CELLS_Y])
 {
     for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {cell[i][j] = 0;}}
 }
 
-void update (int (&cell)[10][10])
+void update (int (&cell)[CELLS_X][CELLS_Y])
 {
-    int updated[10][10];
+    int updated[CELLS_X][CELLS_Y];
     for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {updated[i][j] = cell[i][j];}}
-    for (int i = 1; i < CELLS_X-1; i++) {for (int j = 1; j < CELLS_Y-1; j++) {
+    for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {
         int neighbours[4] = {cell[i-1][j],cell[i+1][j],cell[i][j-1],cell[i][j+1]};
-        if (countNeighbours(returnNeighbours(cell,i,j),1) < 1) {updated[i][j] = 1;}
+        if (countNeighbours(returnNeighbours(cell,i,j),1) < 2) {updated[i][j] = 1;}
         if (countNeighbours(returnNeighbours(cell,i,j),1) > 3) {updated[i][j] = 0;}
     }}
     for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {cell[i][j] = updated[i][j];}}
@@ -59,8 +68,8 @@ int main ()
 
 		ClearBackground(BLACK);
 		for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {
-        if (cell[i][j] == 0) {DrawText(".", 64*i+64, 64*j+64, 24, WHITE);}
-        else if (cell[i][j] == 1) {DrawText("O", 64*i+64, 64*j+64, 24, WHITE);}
+        if (cell[i][j] == 0) {DrawRectangle(32*i+32, 32*j+32, 32, 32, COLORS[0]);}
+        else if (cell[i][j] == 1) {DrawRectangle(32*i+32, 32*j+32, 32, 32, COLORS[1]);}
         }}
 		
 		EndDrawing();
