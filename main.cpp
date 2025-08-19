1798d1d565e15683e9c3d9d1779e508d1ebdfc70
Dominika Mazur <92177055+DominikaMa3ur@users.noreply.github.com>
2025-08-19 22:26:04 +0200

    Vectors
    - non constant size of grid
    - pressing spacebar updates the grid only once until the key is released
    - the colors are temporary?

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "raylib.h"
 #include <climits>
+#include <vector>
 
 const int EDGE_NEIGHBOUR = INT_MAX;
 
-const int CELLS_X = 20;
-const int CELLS_Y = 20;
+int CELLS_X = 128;
+int CELLS_Y = 128;
 
-Color COLORS[2] = {Color{0,0,0,255},Color{255,255,255,255}};
+Color COLORS[2] = {Color{146,236,68,255},Color{182,252,56,255}};
 
 struct NEIGHBOURS4 {
     int TOP;
@@ -24,31 +25,25 @@ int countNeighbours(NEIGHBOURS4 neighbours, int value) {
     return count;
 }
 
-NEIGHBOURS4 returnNeighbours(int (&cell)[CELLS_X][CELLS_Y], int x, int y) {
+NEIGHBOURS4 returnNeighbours(std::vector<std::vector<int>> &cell, int x, int y) {
     NEIGHBOURS4 returned;
-    if (y == 0) returned.TOP = EDGE_NEIGHBOUR;
+    if (y <= 0) returned.TOP = EDGE_NEIGHBOUR;
     else returned.TOP = cell[x][y-1];
-    if (y == CELLS_Y-1) returned.BOTTOM = EDGE_NEIGHBOUR;
+    if (y >= CELLS_Y-1) returned.BOTTOM = EDGE_NEIGHBOUR;
     else returned.BOTTOM = cell[x][y+1];
-    if (x == 0) returned.LEFT = EDGE_NEIGHBOUR;
+    if (x <= 0) returned.LEFT = EDGE_NEIGHBOUR;
     else returned.LEFT = cell[x-1][y];
-    if (x == CELLS_X-1) returned.RIGHT = EDGE_NEIGHBOUR;
+    if (x >= CELLS_X-1) returned.RIGHT = EDGE_NEIGHBOUR;
     else returned.RIGHT = cell[x+1][y];
     return returned;
 }
 
-void fillWithZeros (int (&cell)[CELLS_X][CELLS_Y])
+void update(std::vector<std::vector<int>> &cell)
 {
-    for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {cell[i][j] = 0;}}
-}
-
-void update (int (&cell)[CELLS_X][CELLS_Y])
-{
-    int updated[CELLS_X][CELLS_Y];
+    std::vector<std::vector<int>> updated(CELLS_X, std::vector<int>(CELLS_Y,0));
     for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {updated[i][j] = cell[i][j];}}
     for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {
-        int neighbours[4] = {cell[i-1][j],cell[i+1][j],cell[i][j-1],cell[i][j+1]};
-        if (countNeighbours(returnNeighbours(cell,i,j),1) < 2) {updated[i][j] = 1;}
+        if (countNeighbours(returnNeighbours(cell,i,j),1) < 1) {updated[i][j] = 1;}
         if (countNeighbours(returnNeighbours(cell,i,j),1) > 3) {updated[i][j] = 0;}
     }}
     for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {cell[i][j] = updated[i][j];}}
@@ -56,25 +51,28 @@ void update (int (&cell)[CELLS_X][CELLS_Y])
 
 int main ()
 {
-    int cell[CELLS_X][CELLS_Y];
-    fillWithZeros(cell);
-    const int updateTime = 1.25;
-    float sinceUpdate = 0;
+    std::vector<std::vector<int>> cell(CELLS_X, std::vector<int>(CELLS_Y,0));
 
-	InitWindow(704, 704, "Cellular");
+    const float updateTime = 1.25;
+    float sinceUpdate = 0;
+    bool updateKeyReleased = true;
+
+	InitWindow(640, 640, "Cellular");
+    int square = 640/std::max(CELLS_X,CELLS_Y);
 	
 	while (!WindowShouldClose())
 	{
         float delta = GetFrameTime();
         sinceUpdate += delta;
         while (sinceUpdate >= updateTime) {update(cell); sinceUpdate -= updateTime;}
-        //if (IsKeyDown(KEY_SPACE)) update(cell); // to be improved
+        if (IsKeyDown(KEY_SPACE) && updateKeyReleased) {update(cell); updateKeyReleased = false;}
+        else if (!IsKeyDown(KEY_SPACE)) {updateKeyReleased = true;}
         BeginDrawing();
 
 		ClearBackground(BLACK);
 		for (int i = 0; i < CELLS_X; i++) {for (int j = 0; j < CELLS_Y; j++) {
-        if (cell[i][j] == 0) {DrawRectangle(32*i+32, 32*j+32, 32, 32, COLORS[0]);}
-        else if (cell[i][j] == 1) {DrawRectangle(32*i+32, 32*j+32, 32, 32, COLORS[1]);}
+        if (cell[i][j] == 0) {DrawRectangle(square*i, square*j, square, square, COLORS[0]);}
+        else if (cell[i][j] == 1) {DrawRectangle(square*i, square*j, square, square, COLORS[1]);}
         }}
 		
 		EndDrawing();
